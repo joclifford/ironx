e59956514bc544cb5ccc89b484b5c3e48e905f9c
Jonathan Clifford <jonathan.clifford@eramosa.com>
2020-02-02 21:16:00 -0500

    Added images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import clark from './images/clarky.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>IronX Professionals</p>
+        <img src={clark} className="App-logo" alt="logo" />
+        
       </header>
     </div>
   );
