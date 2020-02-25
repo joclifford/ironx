797b23b1d3fd59c807047576215a676edd7ff7e5
joclifford <jonathanrclifford@gmail.com>
2020-02-25 05:51:19 -0500

    Added new carsouel

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,18 +5,33 @@ import clark from '../../images/clarky.jpg'
 import dumpTruck from '../../images/dumpTruck.jpg'
 import backyard from '../../images/backyard.jpg';
 import frontYard from '../../images/frontYard.jpg';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 let Home = function(){
-
-
        return (
          <div className="App" >
              <div className="App-container">
               <Container> 
+                <Row style={{marginBottom:"100px", marginTop:"50px", marginLeft:"2px;", marginRight:'2px;'}}>
+                  <Carousel infiniteLoop autoPlay swipeable={true}>
+                      <div>
+                        <img src={clark} alt="clark" styles={{width:'50%'}}/>
+                        <p className="legend">TEST</p>
+                      </div>
+                      <div>
+                        <img src={frontYard} alt="clark" />
+                      </div>
+                      <div>
+                        <img src={backyard} alt="clark" />
+                      </div>
+                    </Carousel>
+                </Row>
                  <Row style={{marginBottom:"100px", marginTop:"50px"}}>
                   <ImageCard animation="fade-up" img={dumpTruck} />
                 </Row>
-                <Row className="justify-content-md-center gridRow">
+                <Row className=" gridRow">
+               
+                    {/*  
                     <Col lg={true}>
                       <ImageCard animation="fade-up" img={clark} text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
                     </Col>
@@ -26,8 +41,10 @@ let Home = function(){
                     <Col lg={true}>
                       <ImageCard animation="fade-up" img={backyard} text={"Volutpat consequat mauris nunc congue nisi. Sit amet nisl purus in mollis nunc sed id. Felis bibendum ut tristique et egestas quis ipsum suspendisse. Dictum at tempor commodo ullamcorper. Tortor posuere ac ut consequat semper viverra. Adipiscing tristique risus nec feugiat in. Vulputate mi sit amet mauris. Porttitor massa id neque aliquam vestibulum. Dignissim enim sit amet venenatis urna cursus eget. Suspendisse faucibus interdum posuere lorem. At varius vel pharetra vel turpis nunc eget lorem dolor. In arcu cursus euismod quis viverra nibh. Diam vulputate ut pharetra sit amet aliquam id diam."} />
                     </Col>
+                    */}
                 </Row>
               </Container>
+   
              </div>
          </div>
        )
